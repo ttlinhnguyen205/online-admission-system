dc43406133da8d9d49672e63c9586707900eea51
ttlinhnguyen205 <thuy76864@gmail.com>
2026-09-19 21:57:46 +0700

    init

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -40,14 +40,13 @@ class AppServiceProvider extends ServiceProvider
             app()->isProduction(),
         );
 
-        Password::defaults(fn (): ?Password => app()->isProduction()
-            ? Password::min(12)
+        Password::defaults(fn (): Password =>
+            Password::min(12)
                 ->mixedCase()
                 ->letters()
                 ->numbers()
                 ->symbols()
                 ->uncompromised()
-            : null,
         );
     }
 }
